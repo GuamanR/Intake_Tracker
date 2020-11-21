34e554055a96b086889c366b42b20b7c09e93caf
Richie <richard.a.guaman@gmail.com>
2020-11-20 21:19:47 -0500

    New Project

---
+++ src/main/java/com/tts/intaketracker/IntaketrackerApplication.java
@@ -0,0 +1,13 @@
+package com.tts.intaketracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntaketrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntaketrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/intaketracker/IntaketrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.intaketracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntaketrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
